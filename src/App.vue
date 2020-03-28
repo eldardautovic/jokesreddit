40441b6bb8549f6e31b87c8b92351b73b441c1f9
SalesmanUnknown <eldarcoox123@gmail.com>
2020-03-28 20:59:57 +0100

    Reddit jokes generator

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>/r/jokes</h1>
+    </header>
+    <main v-if="isLoaded">
+      <div class="joke" v-for="joke in jokeDetails" :key="joke.data.id">
+        <a :href="joke.data.url">
+          <h1>{{joke.data.title}}</h1>
+        </a>
+        <div class="joke-author">
+          <p>Author: {{joke.data.author}}</p>
+        </div>
+        <div class="joke-stats">{{joke.data.ups}} Upvotes | {{joke.data.num_comments}} Comments</div>
+      </div>
+      <Footer />
+    </main>
+    <div v-else>
+      <LoadingSpinner />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoadingSpinner from "./components/LoadingSpinner.vue";
+import Footer from "./components/footer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Footer,
+    LoadingSpinner
+  },
+  data() {
+    return {
+      jokeDetails: [],
+      isLoaded: false
+    };
+  },
+  created() {
+    fetch(`https://www.reddit.com/r/Jokes.json?limit=20`)
+      .then(response => response.json())
+      .then(data => {
+        this.jokeDetails = data.data.children;
+        this.jokeDetails.shift();
+        this.isLoaded = true;
+      });
   }
-}
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);
+}
+
+header h1 {
+  min-height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  font-family: sans-serif;
+  color: #2f2f2f;
+  font-size: 50px;
+  padding-top: 50px;
+}
+
+main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.joke {
+  width: 300px;
+  height: 300px;
+  box-shadow: 10px 10px 13px -8px rgba(0, 0, 0, 0.55);
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  justify-content: center;
+  align-content: center;
+  font-family: sans-serif;
+  color: #2f2f2f;
+  margin: 20px;
+  background-color: white;
+}
+
+.joke h1 {
+  font-size: 15px;
+}
+
+.joke a {
+  text-decoration: none;
+  color: currentColor;
+  transition: 0.5s ease;
+}
+
+.joke a:hover {
+  opacity: 0.8;
+}
+
+.joke-author {
+  margin-top: 5%;
+}
+
+.joke-stats {
+  margin-top: 2%;
 }
 </style>
---
+++ src/components/footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="footer">
+    <p>
+      Made with love by
+      <a href="https://salesmanunknown.github.io/">Salesman</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.footer {
+  min-height: 10vh;
+}
+
+.footer p {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  margin-top: 40px;
+  font-family: sans-serif;
+  color: white;
+}
+
+.footer a {
+  padding-left: 5px;
+  text-decoration: none;
+  color: currentColor;
+  transition: 0.7s ease;
+}
+
+.footer a:hover {
+  opacity: 0.7;
+  color: ;
+}
+</style>
